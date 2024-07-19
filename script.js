63f3fe5a99aee852bf53db3fe1a9b459e3304b03
kaisar-rashid <kaisarrashid76@gmail.com>
2024-07-19 11:42:25 +0530

    changed

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // declaring all the needed quantity
   let resetButton = document.querySelector(".Reset");
   let statusButton = document.querySelector(".status");
   let scoreButton = document.querySelector(".score");
   let buttons = document.querySelectorAll(".btn");
   let Display = document.querySelector(".box");
 
+  //declaring all global variable
   let currentPlayer = "X";
   let scoreX = 0;
   let scoreO = 0;
   let gameActive = true;
 
+  // winning possiblities
   const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -21,6 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
     [2, 4, 6],
   ];
 
+  // check for winner
   function checkForWin(buttons, currentPlayer) {
     for (let i = 0; i < winningCombinations.length; i++) {
       const [a, b, c] = winningCombinations[i];
@@ -35,6 +39,8 @@ document.addEventListener("DOMContentLoaded", () => {
     return false;
   }
 
+  // check for Draw
+
   function checkForDraw(buttons) {
     for (let i = 0; i < buttons.length; i++) {
       if (buttons[i].innerText === "") {
@@ -44,15 +50,16 @@ document.addEventListener("DOMContentLoaded", () => {
     return true;
   }
 
+  // Reset Game
   function resetGame() {
     buttons.forEach((btn) => (btn.innerText = ""));
     currentPlayer = "X";
     statusButton.innerText = "";
     Display.style.display = "";
-
     gameActive = true;
   }
 
+  // update score
   function updateScore(player) {
     if (player === "X") {
       scoreX++;
@@ -62,21 +69,23 @@ document.addEventListener("DOMContentLoaded", () => {
     scoreButton.innerText = `Score: X=${scoreX}, O=${scoreO}`;
   }
 
+  //adding eventlisterner
+
   buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
       if (gameActive && btn.innerText === "") {
         btn.innerText = currentPlayer;
 
         if (checkForWin(buttons, currentPlayer)) {
-          statusButton.innerText = `${currentPlayer} wins!`;
+          statusButton.innerText = `${currentPlayer} Wins🤩`;
           updateScore(currentPlayer);
-          Display.style.display = "none";
           gameActive = false;
-        } else if (checkForDraw(buttons)) {
-          statusButton.innerText = "It's a draw!";
           Display.style.display = "none";
-
+        }
+        if (checkForDraw(buttons)) {
+          statusButton.innerText = "Its a Draw😶";
           gameActive = false;
+          Display.style.display = "none";
         } else {
           currentPlayer = currentPlayer === "X" ? "0" : "X";
         }
